442d634813654905df1ddfada0be4804510ca715
Andri Yoga Nur Pratama <andri.pratam@mapan.id>
2023-08-05 12:28:54 +0700

    feat(ui): add multi previews annotation for preview

---
+++ ui/src/main/java/com/ayeee/ui/annotation/MultiPreviews.kt
@@ -0,0 +1,8 @@
+package com.ayeee.ui.annotation
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "Dark Mode", uiMode = UI_MODE_NIGHT_YES)
+@Preview(name = "Light Mode")
+annotation class MultiPreviews
