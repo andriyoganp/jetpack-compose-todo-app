0c1ad3ef39e9caf20cf5a9dc53cc3593a77409da
Andri Yoga Nur Pratama <andri.pratam@mapan.id>
2023-08-05 10:14:26 +0700

    feat(database): update query for get list and delete all

--- database/src/main/java/com/ayeee/database/dao/TaskDao.kt
+++ database/src/main/java/com/ayeee/database/dao/TaskDao.kt
@@ -6,28 +6,36 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Upsert
 import com.ayeee.database.entity.TaskEntity
-import com.ayeee.database.entity.TaskType
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
-    @Query("""
+    @Query(
+        """
+        SELECT * FROM tasks
+        ORDER BY due_date DESC, created_at DESC
+        """
+    )
+    fun getTaskEntities(): Flow<List<TaskEntity>>
+
+    @Query(
+        """
             SELECT * FROM tasks
-            WHERE type = :type
-            OR due_date = :dueDate
-            ORDER BY due_date DESC
-            """)
-    fun getTaskEntities(type: TaskType, dueDate: Long) : Flow<List<TaskEntity>>
+            WHERE due_date IS NOT NULL AND due_date > :dueDate - 86400000 AND due_date <= :dueDate
+            ORDER BY due_date DESC, created_at DESC
+            """
+    )
+    fun getTaskEntities(dueDate: Long): Flow<List<TaskEntity>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertOrIgnoreTask(task: TaskEntity)
+    suspend fun insertOrIgnoreTask(task: TaskEntity): Long
 
     @Upsert
     suspend fun upsertTask(task: TaskEntity)
 
-    @Query("""
-        DELETE FROM tasks
-        WHERE id = :id
-    """)
-    suspend fun deleteTask(id: Long)
+    @Query("""DELETE FROM tasks WHERE id = :id""")
+    suspend fun deleteTask(id: Long): Int
+
+    @Query("""DELETE FROM tasks""")
+    suspend fun deleteAll(): Int
 }
